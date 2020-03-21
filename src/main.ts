1f1a371f9403b9e26a30330cecf25eb491851ce9
chacham <chacham@linecorp.com>
2020-03-21 17:09:06 +0900

    Use buyfy

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import VueSocketIOExt from 'vue-socket.io-extended';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
 
 import App from './App.vue';
 import router from './router';
@@ -9,6 +11,7 @@ import io from './socket';
 Vue.config.productionTip = false;
 
 Vue.use(VueSocketIOExt, io);
+Vue.use(Buefy);
 
 new Vue({
   router,
